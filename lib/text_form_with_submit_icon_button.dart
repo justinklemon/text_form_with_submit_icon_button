3ad08f795eed8534b65b151c72549afdc394e376
Justin Lemon <justinklemon@gmail.com>
2024-02-02 10:42:56 -0700

    Initial Commit

---
+++ lib/text_form_with_submit_icon_button.dart
@@ -0,0 +1,53 @@
+library text_form_with_submit_icon_button;
+
+import 'package:flutter/material.dart';
+
+final GlobalKey<FormFieldState> _formKey = GlobalKey<FormFieldState>();
+
+class TextFormWithIconSubmit extends StatelessWidget {
+  final Function(String) onSubmit;
+  final InputDecoration decoration;
+  final String? Function(String?)? validator;
+  final IconData submitIcon;
+  final String? initialValue;
+  TextFormWithIconSubmit({
+    super.key,
+    required this.onSubmit,
+    required this.decoration,
+    this.submitIcon = Icons.add,
+    this.validator,
+    this.initialValue,
+  }) : _groupNameController = TextEditingController(text: initialValue);
+
+  final TextEditingController _groupNameController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: TextFormField(
+            key: _formKey,
+            controller: _groupNameController,
+            decoration: decoration,
+            validator: validator,
+            onSaved: (value) {
+              if (value == null || value.isEmpty) return;
+              onSubmit(value);
+              _groupNameController.clear();
+            },
+          ),
+        ),
+        IconButton(
+          icon: Icon(submitIcon),
+          onPressed: () {
+            bool isValid = _formKey.currentState?.validate() ?? false;
+            if (isValid) {
+              _formKey.currentState?.save();
+            }
+          },
+        ),
+      ],
+    );
+  }
+}
